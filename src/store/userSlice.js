910ca8a5dcc7f1c7a7baee44ed843cf96cb0662a
Eldar <tmshulk@gmail.com>
2022-11-27 06:59:26 +0500

    second commit

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from "axios"
 
-const API_URL = "http://localhost:3001/api";
+const API_URL = "https://kindly-spring-production.up.railway.app/api";
 
 export const getUsers = createAsyncThunk(
   "user/getUsers",
